386aa1c4da1d5483e34919ce64a381664afc4cfd
Jonguk Lee <ljay.mori@gmail.com>
2022-09-17 02:20:26 +0900

    Add first page

---
+++ src/first/First.js
@@ -0,0 +1,16 @@
+import firstImage from '../images/img_main.png'
+import './First.css';
+
+function First() {
+    return (
+        <div className="First">
+            <header className="First-header">
+                <div id="First-message">
+                    Hello world!
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default First;
