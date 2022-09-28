b27473ce80dcb10d02c11e18588874de3174f368
Jonguk Lee <ljay.mori@gmail.com>
2022-09-28 21:35:38 +0900

    Add react-calendar

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+import ReactCalendar from "react-calendar";
+import moment from "moment";
 import "./Calendar.css";
 
 function Calendar() {
-    return (
-        <div className="Calendar">
-            <header className="Calendar-header">
-                <div>
-                    Hello Calendar!
-                </div>
-            </header>
+  return (
+    <div className="Calendar">
+      <header className="Calendar-header">
+        <div>
+          <ReactCalendar
+            defaultValue={new Date(2023, 0, 7)}
+            formatDay={(locale, date) => date.toLocaleString('en', {day : 'numeric'})}
+            showNeighboringMonth={false}
+            calendarType='US'
+            prevLabel={null}
+            prev2Label={null}
+            nextLabel={null}
+            next2Label={null}
+          />
         </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default Calendar;
